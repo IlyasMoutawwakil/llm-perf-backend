88c6b3189346b8f8e76fc17113b98e37f4b522f4
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2024-01-02 15:08:05 +0100

    rename fields

--- scripts/update_open_llm.py
+++ scripts/update_open_llm.py
@@ -6,13 +6,13 @@ import pandas as pd
 
 TOKEN = os.environ.get("HF_TOKEN", None)
 ALL_COLUMNS = {
-    "Type": "type",
-    "Model": "model",
-    "Precision": "precision",
-    "Average ⬆️": "score",
-    "Hub ❤️": "likes",
-    "#Params (B)": "size",
-    "Hub License": "licence",
+    "Type": "Type",
+    "Model": "Model",
+    "Precision": "Precision",
+    "Average ⬆️": "Score",
+    "Hub ❤️": "Likes",
+    "#Params (B)": "Size",
+    "Hub License": "Licence",
 }
 
 
@@ -31,42 +31,43 @@ def get_model_arch(model_name):
 
 def process_open_llm():
     df = pd.read_csv("open-llm-leaderboard.csv")
-    print(f"Found {len(df)} models in the Open-LLM leaderboard")
 
     # Rename columns
     df.rename(columns=ALL_COLUMNS, inplace=True)
     df = df[ALL_COLUMNS.values()]
 
+    df.dropna(subset=["Model", "Score"], inplace=True)
+    print(f"Found {len(df)} models in the Open-LLM leaderboard")
+
     # Filter out fine-tuned variants
-    df = df[df["type"] == "pretrained"]
-    df.dropna(subset=["model", "score"], inplace=True)
+    df = df[df["Type"] == "pretrained"]
+    print(f"Found {len(df)} models after filtering pretrained variants")
 
     # Filter out quantized variants (we do that ourselves)
-    df = df[~df["precision"].isin(["GPTQ"])]
-    df.sort_values(by="precision", ascending=False, inplace=True)
-    df.drop_duplicates(subset=["model"], keep="first", inplace=True)
-    print(f"Found {len(df)} models after filtering fine-tuned and quantized variants")
+    df = df[~df["Precision"].isin(["GPTQ"])]
+    df.sort_values(by="Precision", ascending=False, inplace=True)
+    df.drop_duplicates(subset=["Model"], keep="first", inplace=True)
+    print(f"Found {len(df)} models after filtering quantized variants")
 
     # Get model architecture
-    df["arch"] = df["model"].apply(get_model_arch)
+    df["Arch"] = df["Model"].apply(get_model_arch)
 
     # sort by score
-    df.sort_values(by=["score"], ascending=False, inplace=True)
-    print(df.head())
+    df.sort_values(by=["Score"], ascending=False, inplace=True)
 
     return df
 
 
 def main():
     processed_open_llm_df = process_open_llm()
-    processed_open_llm_df.to_csv("open_llm.csv", index=False)
+    processed_open_llm_df.to_csv("open-llm.csv", index=False)
 
     if len(processed_open_llm_df) > 0:
         HfApi().upload_file(
             commit_message="Update open models list",
             repo_id="optimum/llm-perf-dataset",
-            path_or_fileobj="open_llm.csv",
-            path_in_repo="open_llm.csv",
+            path_or_fileobj="open-llm.csv",
+            path_in_repo="open-llm.csv",
             repo_type="dataset",
             token=TOKEN,
         )
